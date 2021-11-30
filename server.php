f99b42a560c65161cffd76d57796c0805a5802e1
Nauxx <54441042+Nauxx@users.noreply.github.com>
2021-11-30 12:16:45 +0100

    Update server.php

--- server.php
+++ server.php
@@ -1,10 +1,29 @@
 <?php
 /* header('Content-Type: text/event-stream'); */
 // recommended to prevent caching of event data.
-header('Cache-Control: no-cache'); 
+//header('Cache-Control: no-cache'); 
 
-$rp = file_get_contents('php://input', true);
-echo $rp;
+
+
+$url="https://koch-essen.de/page-sitemap.xml";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $url);    // get the url contents
+
+$data = curl_exec($ch); // execute curl request
+curl_close($ch);
+
+$p = xml_parser_create();
+
+xml_parse_into_struct($p, $data, $vals, $index);
+xml_parser_free($p);
+
+//$xml = simplexml_load_string($data);
+print_r($vals[0]['URLSET']);
+echo PHP_EOL;
+//print_r($index);
+
+
  
 /* function send_message($id, $message, $progress) {
     $d = array('message' => $message , 'progress' => $progress);
